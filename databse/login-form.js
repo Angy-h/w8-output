900b731c6976b3bc36fee6930aea3b4c644ca194
Annaheim.h@gmail.com <Anna Hiragi>
2023-08-31 19:49:57 +0900

    Passwordの自動入力を実装

--- databse/login-form.js
+++ databse/login-form.js
@@ -1,12 +1,19 @@
 // Check if email is stored in localStorage and autofill the form
 const storedEmail = localStorage.getItem("storedEmail");
+const storedPassword = localStorage.getItem("storedPassword");
+
 if (storedEmail) {
   document.getElementById("email").value = storedEmail;
 }
+if (storedPassword) {
+  document.getElementById("password").value = storedPassword;
+}
 
 const signInButton = document.getElementById("signInButton");
-signInButton.addEventListener("click", function (event) {
+signInButton.addEventListener("click", (event) => {
   // Save the email to localStorage when "Sign In" is clicked
   const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
   localStorage.setItem("storedEmail", emailInput.value);
+  localStorage.setItem("storedPassword", passwordInput.value);
 });
