7842a56cd43269e39f499910ed12aabf304de461
Annaheim.h@gmail.com <Anna Hiragi>
2023-08-31 19:32:20 +0900

    LocalStrageのstoredEmailに、emailアドレスの保存を実装

--- databse/login-form.js
+++ databse/login-form.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+// Check if email is stored in localStorage and autofill the form
+const storedEmail = localStorage.getItem("storedEmail");
+if (storedEmail) {
+  document.getElementById("email").value = storedEmail;
+}
+
+const signInButton = document.getElementById("signInButton");
+signInButton.addEventListener("click", function (event) {
+  // Save the email to localStorage when "Sign In" is clicked
+  const emailInput = document.getElementById("email");
+  localStorage.setItem("storedEmail", emailInput.value);
+});
